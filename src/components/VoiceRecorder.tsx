f37c7a33b7963fbe443f4fd52c57ea3412e4666e
abhijithvs680 <abhijithvs680@gmail.com>
2026-07-28 13:38:11 +0530

    fix: send single edited facility wrapped in array as raw json to workflow update receiver

--- src/components/VoiceRecorder.tsx
+++ src/components/VoiceRecorder.tsx
@@ -261,7 +261,7 @@ export const VoiceRecorder = ({
         const response = await fetch('https://innov-dev.beta.injomo.com/workflow.trigger/bankagentsdataupdatereciever6a6855702d36a', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(updatedFacility)
+          body: JSON.stringify([updatedFacility])
         });
 
         if (!response.ok) {
